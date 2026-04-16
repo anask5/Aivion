04988127aafaba554fb66be77526f891cf5ad97c
Anas Khan <leobatesinc@gmail.com>
2026-04-16 17:32:03 +0530

    fixed responsiveness, nav

--- js/aboutus.js
+++ js/aboutus.js
@@ -10,24 +10,25 @@ fetch("footer.html")
 const buttons = document.querySelectorAll('.container-name button');
 
 buttons.forEach(button => {
-    button.addEventListener('click', function(e) {
+    button.addEventListener('click', function (e) {
         e.stopPropagation();
 
         const dropdown = this.nextElementSibling;
         dropdown.classList.toggle('show');
     });
 });
-const toggleBtn = document.getElementById('communityToggle');
-const allDropdowns = document.querySelectorAll('.container-name .dropdown');
 
-let allOpen = false;
+//--------------------------------------------------------------
+const btn = document.getElementById("toggle");
+const sidebar = document.getElementById("sidebar1");
+const close = document.getElementById("closeSidebar");
 
-toggleBtn.addEventListener('click', function(e) {
-    e.preventDefault();
+btn.onclick = () => {
+    sidebar.classList.toggle("active");
+    overlay.classList.toggle("active");
+};
 
-    allOpen = !allOpen;
-
-    allDropdowns.forEach(drop => {
-        drop.classList.toggle('show', allOpen);
-    });
-});
+close.onclick = () => {
+    sidebar.classList.remove("active");
+    overlay.classList.remove("active");
+}
